ea7b6e49546f01e2486ba39aa743ec26d0451624
roman.manko <romanmanco@gmail.com>
2021-08-16 19:34:59 +0300

    1.5. Поля объекта [#166083 #188782]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -4,19 +4,19 @@ public class Cat {
 
     private String name;
 
+    private String food;
+
     public void giveNick(String nick) {
         this.name = nick;
     }
 
-    private String food;
+    public void eat(String meat) {
+        this.food = meat;
+    }
 
     public void show() {
         System.out.println(this.name);
         System.out.println(this.food);
-    }
-
-    public void eat(String meat) {
-        this.food = meat;
     }
 
     public String sound() {
