cc07e21e739c62ed26db175d5bf7165e6ca3d269
roman.manko <romanmanco@gmail.com>
2021-08-17 18:47:26 +0300

    2.1. Рефакторинг - Расстояние между точками. [#122587 #190669]

---
+++ src/test/java/ru/job4j/tracker/PointTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import ru.job4j.oop.Point;
+
+import static org.hamcrest.Matchers.closeTo;
+import static org.junit.Assert.assertThat;
+
+public class PointTest {
+    @Test
+    public void aToB() {
+        Point a = new Point(1, 2);
+        Point b = new Point(4, 2);
+        double rsl = a.distance(b);
+        assertThat(rsl, closeTo(3, 0.1));
+    }
+
+    @Test
+    public void aToC() {
+        Point a = new Point(1, 2);
+        Point c = new Point(3, 5);
+        double rsl = a.distance(c);
+        assertThat(rsl, closeTo(3.6, 0.1));
+    }
+
+    @Test
+    public void cToB() {
+        Point c = new Point(3, 5);
+        Point b = new Point(4, 2);
+        double rsl = c.distance(b);
+        assertThat(rsl, closeTo(3.1, 0.1));
+    }
+}
