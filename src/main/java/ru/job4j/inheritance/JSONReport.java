88bcbb50d60116ff006384c456422c5e28d12d83
roman.manko <romanmanco@gmail.com>
2021-08-31 18:41:02 +0300

    4. Переопределение [#174890 #194070]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,8 +4,9 @@ public class JSONReport extends TextReport{
 
     @Override
     public String generate(String name, String body) {
-        return "name : " + name +
+        return "name : " + name + "," +
                 System.lineSeparator() +
                 "body : " + body ;
     }
 }
+
