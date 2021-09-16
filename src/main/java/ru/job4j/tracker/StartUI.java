e2153045933c79f34070c7160e34b496311bc294
roman.manko <romanmanco@gmail.com>
2021-09-16 13:36:28 +0300

    6. Date. Отображение даты. [#310017 #194102]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -4,12 +4,10 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class StartUI extends Item{
-
     public void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        String currentDateTimeFormat = Item.getCreated().format(formatter);
+        String currentDateTimeFormat = new Item().getCreated().format(formatter);
         LocalDateTime currentDateTime = LocalDateTime.now();
-
         System.out.println("Текущие дата и время до форматирования: " + currentDateTime);
         System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
     }
