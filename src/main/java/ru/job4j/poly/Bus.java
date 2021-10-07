87408bfa664dac9b8d7cadeb64cafe070f573f08
roman.manko <romanmanco@gmail.com>
2021-10-07 17:54:43 +0300

    3. Интерфейс Input [#181092 #211071]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,8 +1,33 @@
 package ru.job4j.poly;
 
-public class Bus implements Vehicle{
+public class Bus implements Transport, Vehicle {
+
+//    private void oilPrice(int price) {
+//    }
+//
+//    private void oilValue(int value) {
+//    }
+
+
+    @Override
+    public void went() {
+        System.out.println("Автобус едет в краснодар.");
+    }
+
+    @Override
+    public void passenger(int numPas) {
+        passenger(24);
+    }
+
+    @Override
+    public int refuel(int oilValue, int oilPrice) {
+//        oilPrice(55);
+//        oilValue(543);
+        return oilValue;
+    }
+
     @Override
     public void move() {
-        System.out.println("Автобус едет по дороге.");
+        System.out.println("Автобус едет.");
     }
 }
