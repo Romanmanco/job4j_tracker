8963e5e54ef9a30ea138215d2d69dd1726dd1727
Romanmanco <romanmanco@gmail.com>
2021-10-26 08:34:44 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -0,0 +1,23 @@
+package ru.job4j.ex;
+
+public class ElementNotFoundException extends Exception {
+    public class FindEl {
+        public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+            int rsl = -1;
+            for (int i = 0; i < value.length; i++) {
+                if (value[i] == key) {
+                    return i;
+                }
+            }
+            return rsl;
+        }
+
+        public static void main(String[] args) {
+            try {
+                indexOf(new String[]{"[12]"}, "22");
+            } catch (ElementNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
