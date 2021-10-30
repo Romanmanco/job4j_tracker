69be825b883fab6bf77392f166ac3c9b7c04e162
Romanmanco <romanmanco@gmail.com>
2021-10-30 20:10:39 +0300

    0.4. Пользовательские исключения. [#219367 #220651]

--- src/main/java/ru/job4j/ex/ElementNotFoundException.java
+++ src/main/java/ru/job4j/ex/ElementNotFoundException.java
@@ -3,16 +3,12 @@ package ru.job4j.ex;
 public class ElementNotFoundException extends Exception {
     public class FindEl {
         public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-            int rsl = -1;
             for (int i = 0; i < value.length; i++) {
                 if (value[i].equals(key)) {
                     return i;
                 }
-                if (rsl == -1) {
-                    throw new IllegalArgumentException("Number should be more or equals than null.");
-                }
             }
-            return rsl;
+            throw new IllegalArgumentException("Number should be more or equals than null.");
         }
 
         public static void main(String[] value, String key) {
