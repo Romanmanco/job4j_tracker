6135255840aed6ce6e755a5c8ba40b89ade5df4f
roman.manko <romanmanco@gmail.com>
2021-11-23 07:45:59 +0300

    1. Телефонный справочник на базе ArrayList [#41598 #231437]

--- src/test/java/ru/job4j/tracker/seach/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/tracker/seach/PhoneDictionaryTest.java
@@ -9,6 +9,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 public class PhoneDictionaryTest {
+
     @Test
     public void whenFindByName() {
         PhoneDictionary phones = new PhoneDictionary();
@@ -17,5 +18,29 @@ public class PhoneDictionaryTest {
         );
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenNotFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Dmitry", "Fursa", "456675", "Moscow")
+        );
+        try {
+            ArrayList<Person> persons = phones.find("Petr");
+            assertThat(persons.get(0).getName(), is("Dmitry"));
+        } catch (IndexOutOfBoundsException ioobe) {
+            System.out.println("Пользователя нет в списке.");
+        }
+    }
+
+    @Test
+    public void whenEmpty() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Denis", "Budeny", "456675", "Moscow")
+        );
+        ArrayList<Person> persons = phones.find("Maxim");
+        assertThat(persons.isEmpty(), is(true));
     }
 }
