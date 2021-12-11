83868cdc5c8e152916023480a6adc2e7496f745a
roman.manko <romanmanco@gmail.com>
2021-12-11 07:08:42 +0300

    0. Сортировка [#10096 #238450]

--- src/main/java/ru/job4j/tracker/item/ItemDescByName.java
+++ src/main/java/ru/job4j/tracker/item/ItemDescByName.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 public class ItemDescByName implements Comparator<Item> {
 
     @Override
-    public int compare(Item second, Item first) {
-        return first.getName().compareTo(second.getName());
+    public int compare(Item first, Item second) {
+        return second.getName().compareTo(first.getName());
     }
 }
