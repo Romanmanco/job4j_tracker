cbcb6bee5716f2a45fe8719d2b4bf85aaa44237f
roman.manko <romanmanco@gmail.com>
2021-12-12 08:20:57 +0300

    2. Комбинированный компаратор [#10036 #238577]

--- src/test/java/ru/job4j/tracker/collection/JobTest.java
+++ src/test/java/ru/job4j/tracker/collection/JobTest.java
@@ -9,21 +9,23 @@ import static org.junit.Assert.assertThat;
 
 public class JobTest {
     @Test
-    public void whenCompatorByNameAndPrority() {
-        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+    public void whenCompDescByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().
+                thenComparing(new JobDescByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("Impl task", 0),
-                new Job("Fix bug", 1)
+                new Job("CC", 1),
+                new Job("CC", 0)
         );
         assertThat(rsl, lessThan(0));
     }
 
     @Test
-    public void whenCompByAscNameAndPrority() {
-        Comparator<Job> cmpNamePriority = new JobAscByName().thenComparing(new JobAscByPriority());
+    public void whenCompByAscNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobAscByName().
+                thenComparing(new JobAscByPriority());
         int rsl = cmpNamePriority.compare(
-                new Job("A", 0),
-                new Job("BB", 1)
+                new Job("AA", 0),
+                new Job("AA", 1)
         );
         assertThat(rsl, lessThan(0));
     }
