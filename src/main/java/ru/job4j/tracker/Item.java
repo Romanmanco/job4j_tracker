5cb60b0e6f94dd01b20b8e7f20d563eec3906992
Romanmanco <romanmanco@gmail.com>
2021-12-14 21:19:26 +0300

    2. Комбинированный компаратор [#10036 #238577]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -8,7 +8,7 @@ public class Item implements Comparable<Item> {
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
     private int id;
     private String name;
-    private LocalDateTime created = LocalDateTime.now();
+    private final LocalDateTime created = LocalDateTime.now();
     private int priority;
 
     public Item() {
